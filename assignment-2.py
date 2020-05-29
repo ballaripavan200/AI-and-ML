24b27a950fa736f2dc8549a80544f47cd38c4a54
ballaripavan200 <50404148+ballaripavan200@users.noreply.github.com>
2020-05-29 10:29:15 +0530

    Add files via upload

--- assignment-2.py
+++ assignment-2.py
@@ -1,17 +1,10 @@
-v = 0;  
-c = 0;  
-s = 0;
-str=input("enter the string")
-str = str.lower();  
-for i in range(0,len(str)):     
-    if str[i] in ('a',"e","i","o","u"):  
-        v += 1;  
-    elif (str[i] >= 'a' and str[i] <= 'z'):  
-        c += 1;
-    else:
-        s+=1
-        
-print("Total number of vowel and consonant are" );  
-print(v);  
-print(c);
-print(s)
+a=input("enter the string")
+n=len(a)
+c=0
+i=0
+while(i<n-1):
+    if a[i]=='a' and a[i+1]=='a':
+        c+=1
+        i=i+1
+    i+=1
+print(c)
