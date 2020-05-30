0af22afb94f5e85f2ee34999b800e7c33360255c
ballaripavan200 <50404148+ballaripavan200@users.noreply.github.com>
2020-05-30 09:42:06 +0530

    Add files via upload

---
+++ assignment-2.6.py
@@ -0,0 +1,11 @@
+i=0
+while i<50:
+    if(i%3==0 and i%5==0):
+        print("FIZZBUZZ")
+    elif i%5==0:
+        print("BUZZ")
+    elif i%3==0:
+        print("FIZZ")
+    else:
+        print(i)
+    i+=1 
